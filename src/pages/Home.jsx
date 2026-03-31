4d93d1d0857ee10dc57679e60ffd11b2a8d6153e
Danny Torres <dannyt8968@gmail.com>
2026-03-31 02:24:55 -0500

    Agregue conversaciones y contactos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,8 +13,13 @@ function Home() {
     return (
         <div>
             <h1>Bienvenido a Velochat</h1>
-            <button onClick={handleLogout}>Cerrar sesion</button>
             <Link to="/profile">Ver mi perfil</Link>
+            <p></p>
+            <Link to="/conversations">Ver conversaciones</Link>
+            <p></p>
+            <Link to="/contacts">Ver contactos</Link>
+            <button onClick={handleLogout}>Cerrar sesion</button>
+            <p></p>
         </div>
     )
 }
